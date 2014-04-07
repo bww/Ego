c8ac7dc228920435fee0b3db145c5c7baff0056c
Brian William Wolter <brian@brianwolter.com>
2014-04-07 12:33:37 -0400

    Initial parser

--- Makefile
+++ Makefile
@@ -2,7 +2,8 @@
 SOURCES=\
 	ego.go \
 	ego_test.go \
-	scanner.go
+	scanner.go \
+	parser.go
 
 all: deps $(SOURCES)
 	go test
---
+++ parser.go
@@ -0,0 +1,69 @@
+// 
+// Copyright (c) 2014 Brian William Wolter, All rights reserved.
+// Ego - an embedded Go parser / compiler
+// 
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+// 
+//   * Redistributions of source code must retain the above copyright notice, this
+//     list of conditions and the following disclaimer.
+// 
+//   * Redistributions in binary form must reproduce the above copyright notice,
+//     this list of conditions and the following disclaimer in the documentation
+//     and/or other materials provided with the distribution.
+//     
+//   * Neither the names of Brian William Wolter, Wolter Group New York, nor the
+//     names of its contributors may be used to endorse or promote products derived
+//     from this software without specific prior written permission.
+//     
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+// IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+// OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+// OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// --
+// 
+// This scanner incorporates routines from the Go package text/scanner:
+// http://golang.org/src/pkg/text/scanner/scanner.go
+// 
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+// 
+// http://golang.org/LICENSE
+// 
+
+package ego
+
+import (
+  _ "fmt"
+)
+
+/**
+ * A parser
+ */
+type parser struct {
+  scanner   *scanner
+}
+
+/**
+ * Create a parser
+ */
+func newParser(s *scanner) *parser {
+  return &parser{s}
+}
+
+/**
+ * Parse
+ */
+func (p *parser) parse() {
+  /// ...
+}
+
+
